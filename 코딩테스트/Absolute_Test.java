26f21b97510fdee9979fb2c36cfd54a371a74ed6
shen5631 <shen5631@naver.com>
2025-10-15 17:04:14 +0900

    Add files via upload

---
+++ 코딩테스트/Absolute_Test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Absolute_Test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int b=0;
+        System.out.print("정수 입력 : ");
+        int a = scan.nextInt();
+
+        if(a<=0){
+            b = a*-1;
+        }
+        else{
+            b=a;
+        }
+        System.out.println(a+"의 절대값은 "+b+"입니다.");
+    }
+}
